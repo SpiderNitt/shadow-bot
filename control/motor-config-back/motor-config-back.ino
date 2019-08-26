9f4bd95e715d4b8e9946cb9707ea5c8177ca9923
Jack Samuel S <jacksamuelnitt@gmail.com>
2019-08-26 21:06:24 +0530

    Updated motor config

--- control/motor-config-back/motor-config-back.ino
+++ control/motor-config-back/motor-config-back.ino
@@ -14,14 +14,13 @@ void setup()
   pinMode(LEnable, OUTPUT);
   pinMode(LFwd, OUTPUT); 
   pinMode(LRev, OUTPUT);
-  //Serial.begin(9600); 
-  delay(3000);
+  //Serial.begin(9600);
     
   }
 void loop()
 {
-    analogWrite(REnable,150);
+    analogWrite(REnable,255);
     digitalWrite(RFwd,HIGH);          
-    analogWrite(LEnable,150);
+    analogWrite(LEnable,255);
     digitalWrite(LFwd,HIGH);
 }
